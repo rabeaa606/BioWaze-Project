8789d4f9f4b08e1fee3d4979ec386d09990b6c26
rabeaa606 <67456826+rabeaa606@users.noreply.github.com>
2021-04-12 15:44:02 +0300

    first commit

---
+++ client/src/app/services/api/api-my-posts.services.ts
@@ -0,0 +1,52 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+import { IPost } from 'src/interfaces';
+import { AppService } from '../services/appServices.services';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ApiMyPostsService {
+    //baseUrl: string = 'http://localhost:4000/apiposts/posts';
+    baseUrl: string;
+
+    mokPosts: IPost[] = [];
+
+    constructor(private http: HttpClient) {
+        this.baseUrl = AppService.baseUrl + 'apiposts/posts';
+    }
+
+    async getPosts(username: string) {
+        await this.http.get<IPost[]>(this.baseUrl + "?username=" + username)
+            .forEach((mapPosts) => {
+                this.mokPosts = mapPosts;
+                this.mokPosts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+
+            });
+        return Promise.resolve(this.mokPosts);
+    }
+
+    insretPost(post: IPost): Observable<IPost> {
+        return this.http.post<IPost>(this.baseUrl, post)
+            .pipe(
+                map((data) => {
+                    return data;
+                }),
+                catchError(this.handleError)
+            );
+    }
+
+    private handleError(error: HttpErrorResponse) {
+        console.error('server error:', error);
+        if (error.error instanceof Error) {
+            let errMessage = error.error.message;
+            return Observable.throw(errMessage);
+            // Use the following instead if using lite-server
+            //return Observable.throw(err.text() || 'backend server error');
+        }
+        return Observable.throw(error || 'Node.js server error');
+    }
+}
